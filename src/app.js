02d4c74ac9d945c8bdfa80912d02fb2fd367f1fd
Mahima N Marathe <mahimanmarathe@gmail.com>
2026-02-10 10:38:41 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,22 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req,res) =>{
-    res.send({firstname : "Mahima", lastname: "Marathe"});
-});
-
-app.post("/user", (req,res) =>{
-    console.log(req.body);
-    res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req,res)=>{
-    res.send("Deleted Successfully");
-});
-
-app.use("/user", (req,res) =>{
-    res.send("hello hello");
-});
+app.get("/user", (req,res,next) =>{
+    console.log("handling resuest 1");
+    // res.send("Response 1");
+    next();
+},
+[(req,res,next)=>{
+    console.log("handling request 2");
+    // res.send("Response 2");
+    next();
+}, 
+(req,res,next) =>{
+    console.log("handling request 3");
+    // res.send("Response 3")
+    next();
+}],
+(req,res) =>{
+    console.log("handling request 4");
+    res.send("Response 4");
+}
+);
 
 app.listen(3000, () =>{
     console.log("server is successfully listening on port 7777");
